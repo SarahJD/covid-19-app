9938248cd665af9a56b4e37ded9f190d08332128
SarahJD <SarahJD@users.noreply.github.com>
2020-11-04 15:35:13 +0100

    added pokemonList with three objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,2 @@
-alert('Hello world');
-
-let favoriteFood = 'chocolate';
-document.write(favoriteFood);
+let pokemonList
+pokemonList=[{name: 'Bulbasaur', height: 7, type: ['grass','poison'],{name: 'Ivysaur', height: 3.03, type: ['grass','poison']},{name: 'Charmander', height: 2, type: 'fire'}];
