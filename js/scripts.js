a3c4b2ebebf3610299a7a8520bb7a18ff0608577
SarahJD <SarahJD@users.noreply.github.com>
2020-11-06 15:26:21 +0100

    added for loop and condition in order to print list-names and remark with regard to height

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 let pokemonList
-pokemonList=[{name: 'Bulbasaur', height: 7, type: ['grass','poison'],{name: 'Ivysaur', height: 3.03, type: ['grass','poison']},{name: 'Charmander', height: 2, type: 'fire'}];
+pokemonList=[
+	{name: 'Bulbasaur', height: 7, type: ['grass','poison']},
+	{name: 'Ivysaur', height: 3.03, type: ['grass','poison']},
+	{name: 'Charmander', height: 2, type: 'fire'}];
+
+for (let i = 0; i < pokemonList.length; i++) { //initialization, condition, action
+	document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")"); // write it on the website
+
+	if (pokemonList[i].height > 6) { //conditional
+		document.write(" - Wow, that's big!"); // write it on the website
+	}
+
+	document.write("<br>"); // for line break when js-file is loaded into html side
+}
