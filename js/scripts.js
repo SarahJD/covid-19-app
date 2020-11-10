83ab065f4d55f9954e3658c439cf0e41f88a2796
SarahJD <SarahJD@users.noreply.github.com>
2020-11-10 09:31:46 +0100

    added an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
-let pokemonList=[
-	{name: 'Bulbasaur', height: 7, type: ['grass','poison']},
-	{name: 'Ivysaur', height: 3.03, type: ['grass','poison']},
-	{name: 'Charmander', height: 2, type: 'fire'}
-];
+// Wrapping global variables in an IIFE
+
+let pokemonRepository = (function() {
+	let pokemonList=[
+		{name: 'Bulbasaur', height: 7, type: ['grass','poison']},
+		{name: 'Ivysaur', height: 3.03, type: ['grass','poison']},
+		{name: 'Charmander', height: 2, type: 'fire'}
+	];
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
+
+	return {
+		getAll: getAll,
+		add: add
+	};
+})();
 
 /* 1st possibility: with a for loop
-for (let i = 0; i < pokemonList.length; i++) { //initialization, condition, action
-	document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "</p>"); // write it on the website
+for (let i = 0; i < pokemonRepository.getAll().length; i++) { //initialization, condition, action
+	document.write("<p>" + pokemonRepository.getAll()[i].name + " (height: " + pokemonRepository.getAll()[i].height + ")" + "</p>"); // write it on the website
 }
 */
 
-/*2nd possibility
-pokemonList.forEach(function(pokemon) {
+
+/* 2nd possibility: with a for each loop and an anonymous function
+pokemonRepository.getAll().forEach(function(pokemon) {
 	document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")");
 	if (pokemon.height > 6) {
 		document.write(" - Wow, that's big!");
@@ -20,7 +38,7 @@ pokemonList.forEach(function(pokemon) {
 });
 */
 
-//3rd possibility
+//3rd possibility: with a for each loop and a named function
 
 function myLoopFunction(pokemon) {
 	document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")");
@@ -29,24 +47,6 @@ function myLoopFunction(pokemon) {
 	};
 	document.write("</p>")
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction); //calling the getAll function in order to retrieve the data inside the IIFE
 
-
-
-
-
-	document.write("<br>"); // for line break when js-file is loaded into html side
-
-
-
-
-/*
-function divide(dividend, divisor) {
-	if (divisor===0) {
-		return "You're trying to divide by zero."
-	}else{
-		let result = dividend / divisor;
-		return result;
-	}
-}
-*/
+document.write("<br>"); // for line break when js-file is loaded into html side
